3a5a1d62956a948873d7b9f55940fb0a0ca1a553
Ronan <rsf.sho@gmail.com>
2018-10-04 22:01:17 -0700

    google auth

--- admin/src/views/auth.vue
+++ admin/src/views/auth.vue
@@ -8,6 +8,27 @@
   import firebase from 'firebase/app'
   import 'firebase/auth';
   export default {
-    name: "auth"
+    name: "auth",
+    mounted() {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user)
+        this.$router.push('/a')
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    }
   }
 </script>
