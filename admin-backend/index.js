509fa6086b63259909c8215695127cd3389d54d0
Ronan <rsf.sho@gmail.com>
2018-10-04 19:14:50 -0700

    super basic atendee uploading

--- admin-backend/index.js
+++ admin-backend/index.js
@@ -1,29 +1,51 @@
 const admin = require('firebase-admin');
 const fs = require('fs');
 const csv = require('fast-csv')
-let stream = fs.createReadStream("./private/data.csv");
-let data = []
+let students = fs.createReadStream("./private/students.csv");
+//let teachers = fs.createReadStream("./private/teachers.csv");
+let studentData = []
+let teacherData = []
 const serviceAccount = require("./private/hyphenhacks-dc851-firebase-adminsdk-au5a6-3d9ad5cb50.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://hyphenhacks-dc851.firebaseio.com"
 });
-var csvStream = csv()
+let db = admin.database();
+let atendeesDB = db.ref("/attendeeDB/attendees");
+let studentsStream = csv()
 .on("data", function(row){
   console.log(row);
-  data.push(row)
+  row.push('student')
+  studentData.push(row)
+  atendeesDB.push().set(row)
 
 })
 .on("end", function(){
   console.log("done");
-  fs.writeFile("./private/data.json", JSON.stringify(data), function(err) {
+  fs.writeFile("./private/students.json", JSON.stringify(studentData), function(err) {
     if(err) {
       return console.log(err);
     }
+    console.log("The file was saved!");
+  });
+});
+let teacherStream = csv()
+.on("data", function(row){
+  console.log(row);
+  row.push('student')
+  teacherData.push(row)
 
+})
+.on("end", function(){
+  console.log("done");
+  fs.writeFile("./private/students.json", JSON.stringify(teacherData), function(err) {
+    if(err) {
+      return console.log(err);
+    }
     console.log("The file was saved!");
   });
 });
 
-stream.pipe(csvStream);
+students.pipe(studentsStream);
+//teachers.pipe(teacherStream);
