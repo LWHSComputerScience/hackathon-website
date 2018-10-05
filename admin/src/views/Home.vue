df1d6879cbfe7e3513bbc8d1e89b6fe6648a240f
Ronan <rsf.sho@gmail.com>
2018-10-05 16:04:36 -0700

    emails

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home page">
     <div class="person" v-for="person in $parent.filterdList">
-      <p>{{person[3]}}</p>
+      <h1 class="person__name">{{person[3]}}</h1>
+      <p class="person__email">{{person[2]}}</p>
     </div>
   </div>
 </template>
