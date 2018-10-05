9558fe448a4129a2e6e38848ce91d7ee72480fda
Ronan <rsf.sho@gmail.com>
2018-10-05 16:01:06 -0700

    even more chuck norris jokes

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -5,7 +5,9 @@
       <input class="nav__search" type="search" placeholder="search">
     </nav>
     <router-view/>
-    <footer v-if="$route.path != '/'" class="app__footer"></footer>
+    <footer v-if="$route.path != '/'" class="app__footer">
+      <p class="footer__quote">{{quote}}</p>
+    </footer>
   </div>
 </template>
 <script>
@@ -13,11 +15,13 @@
   import './assets/global.scss'
   import firebase from 'firebase/app';
   import 'firebase/database'
+
   export default {
     name: 'appContainer',
     data() {
       return {
-        totalList: {}
+        totalList: {},
+        quote: ''
       }
     },
     computed: {
@@ -25,11 +29,19 @@
         return this.totalList
       }
 
+
     },
     mounted() {
       firebase.database().ref('/attendeeDB/attendees/').on('value', (data) => {
         console.log(data.val())
         this.totalList = data.val()
+      })
+      fetch('http://api.icndb.com/jokes/random?escapse=javascript')
+      .then((resp) => resp.json()) // Transform the data into json
+      .then((data) => {
+        console.log(data)
+        this.quote = data.value.joke;
+        // Create and append the li's to the ul
       })
     }
 
