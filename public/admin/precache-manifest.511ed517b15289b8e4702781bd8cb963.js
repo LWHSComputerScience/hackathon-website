d815c7018cf5c07935c755f813626c86345b2615
Ronan <rsf.sho@gmail.com>
2018-10-05 16:13:15 -0700

    added basic production test build

---
+++ public/admin/precache-manifest.511ed517b15289b8e4702781bd8cb963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin/robots.txt"
+  },
+  {
+    "revision": "e30e8bce55654e860be3",
+    "url": "/admin/js/chunk-vendors.5189a3fb.js"
+  },
+  {
+    "revision": "86fdb9027b43526b3ac5",
+    "url": "/admin/js/app.13fb1c24.js"
+  },
+  {
+    "revision": "0e818f712629afad7c6363da259b7472",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "e30e8bce55654e860be3",
+    "url": "/admin/css/chunk-vendors.3fcd6fea.css"
+  },
+  {
+    "revision": "86fdb9027b43526b3ac5",
+    "url": "/admin/css/app.28bd82c2.css"
+  }
+];
